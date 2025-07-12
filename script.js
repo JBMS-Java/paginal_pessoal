a905129c41e822ae0c9acb4046710f3015aab1e4
João Muniz <joaosanata@gmail.com>
2025-07-12 18:40:22 -0300

    Mais um sistema automatizado com JS

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-function atualizarIdade(dataNascimento) {
-    const hoje = new Date();
-    const nascimento = new Date(dataNascimento);
+// Idade automatizada:
 
-let idade = hoje.getFullYear() - nascimento.getFullYear();
+function atualizarIdade(dataNascimento) { //Cria uma função chamada atualizarIdade e ela recebe o parâmetro dataNascimento (aaaa/mm/dd)
+    const hoje = new Date(); //Cria um objeto Date com a data e hora atual do sistema (do momento em que a página foi carregada)
+    const nascimento = new Date(dataNascimento); //Converte a string para um objeto date.
+
+let idade = hoje.getFullYear() - nascimento.getFullYear(); // Calcula o ano subtraindo o atual pelo ano de nascimento
 const aniversarioEsseAno = new Date(hoje.getFullYear(), nascimento.getMonth(), nascimento.getDate());
 
 if(hoje < aniversarioEsseAno) {
@@ -17,4 +19,26 @@ if(hoje < aniversarioEsseAno) {
 
 document.addEventListener("DOMContentLoaded", function() {
     atualizarIdade("2001-05-19");
+})
+
+// Anos como escritor automatizado:
+function atualizarAnosComoEscritor (dataInicio) {
+    const hoje = new Date();
+    const inicio = new Date(dataInicio);
+
+    let anos = hoje.getFullYear() - inicio.getFullYear();
+    const aniversarioCarreira = new Date(hoje.getFullYear(), inicio.getMonth(), inicio.getDate());
+
+    if(hoje < aniversarioCarreira) {
+        anos--
+    }
+
+    const spanAnos = document.getElementById("anos-escritor");
+    if(spanAnos) {
+        spanAnos.textContent = anos;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    atualizarAnosComoEscritor("2019-05-19");
 })
